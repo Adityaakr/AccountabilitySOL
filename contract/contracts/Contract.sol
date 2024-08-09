4a6bc2dcf956218690405c3a07aeae17a4709170
Aditya Kumar <08quant@gmail.com>
2024-08-09 19:39:43 +0530

    first-commit

---
+++ contract/contracts/Contract.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// lock up funds for a period of time
+// users can only unlock funds if they own an NFT from another smart contract
+
+contract MyContract {
+    constructor() {}
+}
